6b9b0d6fc62ab44ddcb4d544c39d3134f9a481cb
courtt <tac136@pitt.edu>
2022-10-24 16:58:27 -0400

    Added EM_avg and improved ne_t functions
    A function to better calculate tau as a function of time for each layer and save for
    every time for ease of use
    A function to calculate EM avg values with the improved tau calc

--- nei.py
+++ nei.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 from glob import glob
+from scipy import integrate
 class NEIData:
     # names = ['igrid'....]
     def __init__(self,exp,amb,model_num):
@@ -22,6 +23,7 @@ class NEIData:
         self.jmax   = None
         self.xion   = None
         self.Ti     = None
+        self.ztau   = None
         self.tau    = None
         self.read_columns(exp,amb,model_num)
 
@@ -46,7 +48,7 @@ class NEIData:
         jmax = np.argmax(ab)
         xion = dat[:,33:330]
         Ti   = dat[:,330:349]
-        tau  = dat[:,349]
+        ztau = dat[:,349]
 
         self.igrid = igrid
         self.age   = age  
@@ -61,14 +63,13 @@ class NEIData:
         self.jmax  = jmax 
         self.xion  = xion 
         self.Ti    = Ti   
-        self.tau   = tau  
+        self.ztau  = ztau  
 
 
 
-    def ne_t_calc(exp,amb,model_num,CD_layer):
+    def ne_t_calc(self,exp,amb,CD_layer):
         '''
         Calculates tau for each layer from the RS to CD for all ages
-        up to specified age 
 
         Parameters
         ----------
@@ -76,79 +77,101 @@ class NEIData:
 
         amb : str, end of directory corresponding to ambient medium and any extras
 
-        model_num: number of output file to which you're integrating 
-        e.g. snr_Ia_prof_a_1065.dat would be 1065
-
         CD_layer : int, layer number of CD in output
         '''
-        infile = np.sort(glob('./'+ exp + '_' + amb + '/output/snr_Ia_prof_a_1*.dat'))
-        target_file = './'+ exp + '_' + amb + '/output/snr_Ia_prof_a_' + str(model_num) +'.dat'
-        target_ind = np.where(infile==target_file)[0][0]
 
-        layer_list= [[] for x in range(CD_layer)]
-        ne_list  = [[] for x in range(CD_layer)]
-        age_list = [[] for x in range(CD_layer)]
-        tau      = []
-        for i in range(len(infile[:target_ind + 1])):
+        infile = np.sort(glob('/Users/travis/pn_spectra/'+exp+'_'+amb+'/output/snr_Ia_prof_a_1*.dat'))
+        yr_to_s = 365*24*3600
+        layer_list  = np.arange(1,CD_layer+1)
+        ne_t_arr    = np.zeros((CD_layer,101))
+        ne_arr      = np.zeros((CD_layer,101))
+        age_arr     = np.zeros(101)
+        # for i in range(len(infile[:target_ind + 1])):
+        for i in range(len(infile)):
+
             dat = np.loadtxt(infile[i])
 
             if len(dat.shape) == 1:
                 dat = dat.reshape([1,len(dat)])
                     
             shocked_layers = dat[:,0]
-        
             ne = dat[:,11]
-            age = dat[:,1]
-            for layer in range(CD_layer):
-                if any(dat[:,0] == layer):
-                    layer_ind = np.where(shocked_layers == layer)[0][0]
-                    layer_list[layer].append(layer_ind)
-                    ne_list[layer].append(ne[layer_ind])
-                    age_list[layer].append(age[layer_ind]*3.154e+7)
-# create a big matrix 562x100 ne and tau
-# age list 1-d array
-# layer list                 
-            
-        for layer in range(CD_layer):
-            if len(age_list[layer]) > 0:
-                if len(age_list[layer]) == 1: 
-                    tau.append(0)
-                else:
-                    tau.append(np.integrate.trapz(ne_list[layer],age_list[layer]))
 
-        return tau # self.tau=tau
+            age_arr[i] = dat[0,1]*yr_to_s
+
+            for j,layer in enumerate(layer_list):
+                if layer in shocked_layers:
+                    ne_arr[j,i] = ne[layer==shocked_layers]
+                else:
+                    ne_arr[j,i] = 0
+
+        ne_t_arr = integrate.cumtrapz(ne_arr,age_arr)
+        return ne_t_arr
+
+
+    def emission_measure_avg(self,exp,amb,model_num,CD_layer,ion):
+        '''
+        Calculates emission measure averages for velocity and ionization timescale for a given age (outfile)
+
+        Parameters
+        ----------
+        base_dir : str, directory name of files 
+
+        CD_layer : int, layer number of CD in output
+
+        out_file_num: number of output file to which you're integrating 
+        e.g. snr_Ia_prof_a_1065.dat would be 1065
+
+        ion      : str, ion symbol for which you want to calculate
+        '''
+        path = os.cwd()
+        infile = path + exp + '_' + amb + '/output/snr_Ia_prof_a_' + str(model_num) +'.dat'
+        n_spec  = 19
+
+        ion_name = ['H','He','C','N','O','Ne','Mg','Si','S','Ar','Ca','Fe','Ni','Na','Al','P','Ti','Cr','Mn']
+        i_spec 	= ion_name.index(ion)  #index of element, e.g., 4 = 'O', 11 = 'Fe'
+        nj  = np.array([1,2,6,7,8,10,12,14,16,18,20,26,28,11,13,15,22,24,25])
+        ind = np.zeros([n_spec])
+        ix  = 0
+        for no in range(n_spec):
+            ixn = ix + nj[no]
+            ind[no] = ix
+            ix = ixn+1
+        ind = np.array(ind,dtype='int') 
+
+        dat = np.loadtxt(infile)
+        if len(dat.shape) == 1:
+            dat = dat.reshape([1,len(dat)])
+
+        i_CD = np.where(dat[:,0] == CD_layer)[0][0]
+        xion = dat[:i_CD+1,33:]
+        ne   = dat[:i_CD+1,11]
+        nH   = dat[:i_CD+1,12]
+        vol  = dat[:i_CD+1,5]
+
+        ab   = 10**(dat[:i_CD+1,14:33]-12.0)
+        vel  = dat[:i_CD+1,8]/1e5 
+        EM_x = ne*nH*ab[:,i_spec]*vol     
+        frac = xion[:i_CD+1,ind[i_spec]:ind[i_spec]+nj[i_spec]+1] #gives array of ion fractions
+
+        charges = [np.arange(nj[i_spec]+1) for i in range(i_CD+1)]
+
+        ionization_state = np.sum(charges*frac,axis=1)/np.sum(frac,axis=1)
+        ne_t_arr = np.array(self.ne_t_calc(exp,amb,CD_layer))
+        tau = ne_t_arr[:,model_num - 1000 - 1] # the -1 is because there is no tau for the first output bc no time elapsed
+
+        ion_s_EM_avg = np.sum(ionization_state*EM_x) / np.sum(EM_x)
+        tau_EM_avg = np.sum(tau*EM_x) / np.sum(EM_x)
+        vel_EM_avg = np.sum(vel*EM_x) / np.sum(EM_x)
+
+        return ion_s_EM_avg, tau_EM_avg, vel_EM_avg
+
+
+
+    
+
 
 # this is how i'll do this later when calling the class
 # hd = HydroData(exp,amb,model_num)
 # hd.rad
-'''
-infile = np.sort(glob('/Users/travis/pn_spectra/ddt40_2p0_uniform/output/snr_Ia_prof_a_1*.dat'))
-target_file = '/Users/travis/pn_spectra/ddt40_2p0_uniform/output/snr_Ia_prof_a_1100.dat'
-target_ind = np.where(infile==target_file)[0][0]
-CD_layer=400 
 
-layer_list  = np.arange(1,CD_layer+1)
-ne_t_arr    = np.zeros((CD_layer,101))
-ne_arr      = np.zeros((CD_layer,101))
-age_arr     = np.zeros(101)
-# for i in range(len(infile[:target_ind + 1])):
-for i in range(len(infile)):
-
-    dat = np.loadtxt(infile[i])
-
-    if len(dat.shape) == 1:
-        dat = dat.reshape([1,len(dat)])
-            
-    shocked_layers = dat[:,0]
-    ne = dat[:,11]
-
-    age_arr[i] = dat[0,1]
-
-    for j,layer in enumerate(layer_list):
-        if layer in shocked_layers:
-            ne_arr[j,i] = ne[layer==shocked_layers]
-        else:
-            ne_arr[j,i] = 0
-
-print(ne_arr[0])
-'''
