1408c95865d386c734421001f041c2319478161e
courtt <tac136@pitt.edu>
2022-12-15 10:06:45 -0500

    re-instituted multipath

--- nei.py
+++ nei.py
@@ -176,7 +176,8 @@ class NEIData:
             ab_rel    = dict(zip(np.arange(1,31),np.zeros(30)))
             popul     = dict(zip(np.arange(1,31),np.zeros(30)))
 
-            for i in range(n_part):
+            def spec_run(i):
+            # for i in range(n_part):
                 xion = self.xion[i]
                 ne   = self.ne[i]
                 nH   = self.nH[i]
@@ -185,18 +186,35 @@ class NEIData:
                 ab   = self.ab[i]
                 jmax = np.argmax(ab)
                 igrid = self.igrid[i]
+                lum_ej = np.zeros(len(E)-1)+1e-50
+                lum_am = np.zeros(len(E)-1)+1e-50
+
+                if (Te < 0.000862*keV/1.381e-16):
+                    return lum_ej, lum_am
+
                 for j in range(n_spec):
                         ab_rel[zatom[j]] = (ab[j]/ab[jmax])/ab_init[zatom[j]]
                         popul[zatom[j]]  = xion[ind[j]:ind[j]+nj[j]+1]
                 nei.abundsetvector = ab_rel
-                                
-                if igrid < i_CD:	#you can output the DEM profile separately by storing ne*ni*vol against rad
-                    lum_ej += ne*nH*ab[jmax]*nei.return_spectrum(Te,1e11,init_pop=popul,freeze_ion_pop=True,teunit='K')*vol #ph/s/bin
-                else:
-                    lum_am += ne*nH*ab[jmax]*nei.return_spectrum(Te,1e11,init_pop=popul,freeze_ion_pop=True,teunit='K')*vol #ph/s/bin
+
+                try:                
+                    if igrid < i_CD:	#you can output the DEM profile separately by storing ne*ni*vol against rad
+                        lum_ej += ne*nH*ab[jmax]*nei.return_spectrum(Te,1e11,init_pop=popul,freeze_ion_pop=True,teunit='K')*vol #ph/s/bin
+                    else:
+                        lum_am += ne*nH*ab[jmax]*nei.return_spectrum(Te,1e11,init_pop=popul,freeze_ion_pop=True,teunit='K')*vol #ph/s/bin
+                except:
+                    print("warning...", igrid, ne, Te, vol)
+
                 sys.stdout.write("\rProgress:%i/%i (particle:%i) (peak Z:%i)"%(i+1,n_part,igrid,zatom[jmax]))
                 sys.stdout.flush()
+
+                return lum_ej, lum_am
                                                               
+            pool = ProcessingPool(nodes=pa.helpers.cpu_count())
+            result = np.array(pool.map(spec_run, range(n_part)))#,dtype=float)
+
+            lum_ej = sum(result[:,0],0) #ph/s/bin
+            lum_am = sum(result[:,1],0)
 
             Ec = 0.5*(E[1:]+E[:-1])
             #lum_ej *= (Ec**2/dE)*1e3*1.602e-12
