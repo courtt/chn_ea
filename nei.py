8ced3ec3b63e68f6c42c8737c3e2b4d33a0d39ce
Travis <tac136@pitt.edu>
2022-12-15 11:23:59 -0500

    working version of spectrum

--- nei.py
+++ nei.py
@@ -185,6 +185,13 @@ class NEIData:
                 ab   = self.ab[i]
                 jmax = np.argmax(ab)
                 igrid = self.igrid[i]
+
+                lum_ej  = np.zeros(len(E)-1)+1e-50 
+                lum_am = np.zeros(len(E)-1)+1e-50
+
+                if (Te < 0.000862*keV/1.381e-16): 
+                        return lum_ej, lum_csm
+
                 for j in range(n_spec):
                         ab_rel[zatom[j]] = (ab[j]/ab[jmax])/ab_init[zatom[j]]
                         popul[zatom[j]]  = xion[ind[j]:ind[j]+nj[j]+1]
